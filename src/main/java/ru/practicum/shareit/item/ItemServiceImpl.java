efec799f8e51f1d8b83249a592100ad6faf52def
Sergei <srg391@yandex.ru>
2022-10-21 21:23:16 +0300

    add-controllers

--- src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
@@ -71,7 +71,7 @@ public class ItemServiceImpl implements ItemService {
                 .orElseThrow(() -> new NotFoundException("Вещь c id=" + itemDto.getId() + " не существует!"));
         final User owner = userRepository.getByUserId(userId)
                 .orElseThrow(() -> new NotFoundException("Хозяин c id=" + userId + " не существует!"));
-        if ((item.getOwner().getId()).equals(owner.getId())) {
+        if (item.getOwner().getId().equals(owner.getId())) {
             if (itemDto.getName() != null) {
                 item.setName(itemDto.getName());
             }

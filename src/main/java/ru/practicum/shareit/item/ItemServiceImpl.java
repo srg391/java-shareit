e3c2c2dea54e89c30ad398965a890454890ae9c8
Sergei <srg391@yandex.ru>
2022-11-19 01:18:13 +0300

    add-bookings

--- src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
@@ -38,9 +38,9 @@ public class ItemServiceImpl implements ItemService {
     public ItemWithBookingDto getItem(long itemId, long userId) {
         final Item item = itemRepository.findById(itemId)
                 .orElseThrow(() -> new NotFoundException("Вещь c id=" + itemId + " не существует!"));
-        if (!item.getOwner().getId().equals(userId)) {
-            return itemMapper.createDtoItemWithBooking(item, null, null);
-        }
+//        if (!item.getOwner().getId().equals(userId)) {
+//            return itemMapper.createDtoItemWithBooking(item, null, null);
+//        }
         return getItemWithBooking(item, userId, itemId);
     }
 
